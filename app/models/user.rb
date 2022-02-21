9e00891d297f496bc6599dbd0be549bc30ca52c8
Oliver <olivercai@uchicago.edu>
2022-02-20 19:06:35 -0600

    Initialized

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :received_friend_requests, class_name: "FriendRequest", foreign_key: "receiver_id"
+  has_many :sent_friend_requests, class_name: "FriendRequest", foreign_key: "sender_id"
+end
---
+++ db/migrate/20220221004702_add_details_to_friend_request.rb
@@ -0,0 +1,8 @@
+class AddDetailsToFriendRequest < ActiveRecord::Migration[7.0]
+  def change
+    change_table(:friend_requests) do |t|
+      t.references :sender, foreign_key: {to_table: 'users'}
+      t.references :receiver, foreign_key: {to_table: 'users'}
+    end
+  end
+end
